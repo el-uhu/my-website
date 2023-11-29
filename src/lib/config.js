7049ded830a074766f2e4a3f2ad3b682ad609b74
Lukas Hutter <el-uhu@users.noreply.github.com>
2023-11-29 19:50:11 +0000

    added config.js file

---
+++ src/lib/config.js
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'eluhu.xyz'
+export const description = 'Writing, Teaching, Visualisation & Beer'
+export const url = dev ? 'http://localhost:5173/' : 'https://eluhu.xyz/'
